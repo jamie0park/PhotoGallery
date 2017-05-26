59ab9f3a7128d6742dc10f30c6f755eed83de9f7
Jamie Y. Park <jamie0park@gmail.com>
2017-05-26 19:04:22 +0900

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/QueryPreferences.java
@@ -0,0 +1,24 @@
+package com.bignerdranch.android.photogallery;
+
+import android.content.Context;
+import android.preference.PreferenceManager;
+
+/**
+ * Created by jamie0park on 26/05/2017.
+ */
+
+public class QueryPreferences {
+    private static final String PREF_SEARCH_QUERY = "searchQuery";
+
+    public static String getStoredQuery(Context context) {
+        return PreferenceManager.getDefaultSharedPreferences(context)
+                .getString(PREF_SEARCH_QUERY, null);
+    }
+
+    public static void setStoredQuery(Context context, String query) {
+        PreferenceManager.getDefaultSharedPreferences(context)
+                .edit()
+                .putString(PREF_SEARCH_QUERY, query)
+                .apply();
+    }
+}
